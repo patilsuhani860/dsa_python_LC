487637802be8abb28b55da0d171cd782efddfdc5
patilsuhani860 <patilsuhani860@gmail.com>
2026-06-21 23:53:06 +0530

    168. Excel sheet column title

---
+++ string/Easy7.py
@@ -0,0 +1,13 @@
+import math
+
+class Solution(object):
+    def convertToTitle(self, columnNumber):
+        result = []
+        
+        while columnNumber > 0:
+            columnNumber -= 1  
+            remainder = columnNumber % 26
+            result.append(chr(65 + remainder))
+            columnNumber //= 26
+            
+        return "".join(reversed(result))
